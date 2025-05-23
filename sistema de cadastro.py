19a1ad3293f0637c8f0d744c35ea13687e8309c8
VictorEmanoel97 <oliveirapreto45@gmail.com>
2025-05-23 10:21:18 -0300

    Add files via upload

--- sistema de cadastro.py
+++ sistema de cadastro.py
@@ -8,10 +8,10 @@ class Estudante:
          return f"Nome: {self.nome}, Cidade: {self.cidade}, Transporte: {self.transporte}"
 def adicionar_cadastro():
     nome_user = input("digite seu nome:\n")
-    moradia_user = input("onde você mora?")
-    transporte_user = input("você tem tansporte pra vir a escola?").lower()
+    moradia_user = input("onde você mora?\n")
+    transporte_user = input("você tem tansporte pra vir a escola?\n").lower()
     if transporte_user =="sim":
-        transporte_tipo = input("qual o tipo de transporte?")
+        transporte_tipo = input("qual o tipo de transporte?\n")
         estudante_obj = Estudante(nome_user, moradia_user, transporte_tipo)
         informacoes.append(estudante_obj) 
         print("Obrigado por responder o questionario")
@@ -21,9 +21,9 @@ def adicionar_cadastro():
         informacoes.append(estudante_obj)
     else:
         print("resposta invalida")
-        estudante_obj = None
+
         
-def cadastros():
+def conferir_cadastros():
     if informacoes == []:
         print("A lista esta vazia")
     else:
@@ -32,15 +32,18 @@ def cadastros():
             
 informacoes = []
 while True:
-    print("1. cadastrar pessoa")
-    print("2. conferir cadastro")
-    print("3. sair")
-    r = input("oque deseja fazer\n")
-    if r == "1":
+   
+    print("-------------------------")
+    print("||1. cadastrar pessoa  ||")
+    print("||2. conferir cadastros||")
+    print("||3. sair              ||")
+    print("-------------------------")
+    res = input("oque deseja fazer?\n")
+    if res == "1":
         adicionar_cadastro()
-    elif r == "2":
-        cadastros()
-    elif r == "3":
+    elif res == "2":
+        conferir_cadastros()
+    elif res == "3":
         print("saindo....")
         break
     else:
